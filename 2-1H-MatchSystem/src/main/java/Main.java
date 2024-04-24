38c725ddbc15830efcb86c61dc7ffe5623014765
iamsherry000 <39553867+iamsherry000@users.noreply.github.com>
2024-04-24 08:25:24 +0000

    create individuals list

--- 2-1H-MatchSystem/src/main/java/Main.java
+++ 2-1H-MatchSystem/src/main/java/Main.java
@@ -1,7 +1,11 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    private static final String[] habits = {"dancing", "music", "boardgame", "online games", "coding", "movie", "KTV","swimming","hiking"};
+    private static final int numberOfIndividuals = 10; 
 
+    public static void main(String[] args) {
+        List<Individual> individuals = new ArrayList<>();
     }
 }
