0889db55971a1fe3a16fa5fc3b6260db2ff25beb
iamsherry000 <39553867+iamsherry000@users.noreply.github.com>
2024-04-25 09:23:40 +0000

    some code of main.java

--- 2-1H-MatchSystem/src/main/java/Main.java
+++ 2-1H-MatchSystem/src/main/java/Main.java
@@ -3,9 +3,38 @@ import java.util.List;
 
 public class Main {
     private static final String[] habits = {"dancing", "music", "boardgame", "online games", "coding", "movie", "KTV","swimming","hiking"};
-    private static final int numberOfIndividuals = 10; 
+    private static final int numbOfIndividuals = 10; 
 
     public static void main(String[] args) {
-        List<Individual> individuals = new ArrayList<>();
+        List<Individual> individualsList = new ArrayList<>();
+
+        for(int i = 0; i < numbOfIndividuals; i++){
+            individualsList.add(new Individual(i+1, 
+                                getRandomBoolean(), 
+                                getRandomInt(),
+
+                                ))
+        }
     }
+}
+
+// generate random gender with 0 for male, 1 for female.
+public static boolean getRandomBoolean() {
+    return Math.random() < 0.5;
+}
+
+// generate random age between maxAge and minAge.
+public static int getRandomInt() { 
+    final int maxAge = 80;
+    final int minAge = 18;
+    int age = 0;
+    return age = (int)(Math.random() * (maxAge - minAge)) + minAge;
+}
+
+// generate random habits 
+public static Set<String> randomHabits() {
+    // int totalHabitsNum = habits.length + 1;
+    // int size = (int)(Math.random() * (habits.length - 1))
+    int size = (int)(Math.random() * habits.length);
+    
 }
