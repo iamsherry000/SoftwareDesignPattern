9cffd5fb8087f72bc1587d477fd75fc4eabc2526
Sherry Tseng <iamsherry000@gmail.com>
2025-08-15 13:32:23 +0800

    observer pattern homework

--- src/ch3_complexBV/ObserverPattern/YT_User.java
+++ src/ch3_complexBV/ObserverPattern/YT_User.java
@@ -2,5 +2,5 @@ package ch3_complexBV.ObserverPattern;
 
 public interface YT_User {
     String getName();
-    void update(Video video); // 若要更多資訊，可用 Notification event
+    void update(YT_Channel channel, Video video); // 若要更多資訊，可用 Notification event
 }
