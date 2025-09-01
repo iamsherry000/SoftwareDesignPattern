eeabfb9958babbcb3121aa78fd19ff2623ea5f1b
Sherry Tseng <iamsherry000@gmail.com>
2025-09-01 17:02:37 +0800

    adjust a bit of main

--- src/ch3_complexBV/CommandPattern/Main.java
+++ src/ch3_complexBV/CommandPattern/Main.java
@@ -19,13 +19,24 @@ public class Main {
             if (1 == input){
                 System.out.println("設置巨集指令 (y/n)：");
                 char c = in.next().charAt(0); // 讀取 y/n
+                if(c == 'y' || c == 'Y'){
+                    // todo: 設定巨集指令
+                }
+                else if(c == 'n' || c == 'N'){
+                    System.out.printf("請輸入快捷鍵 (a-z)： ");
+                    char key = in.next().charAt(0); // 讀取 a-z
+                    controller.press(key);
+                }
+
             } else if (2 == input) {
                 controller.undo();
             } else if (3 == input) {
                 controller.redo();
             } else {
-                controller.press(input);
+                System.out.println("不在選項內");
             }
         }
     }
+
+    
 }
