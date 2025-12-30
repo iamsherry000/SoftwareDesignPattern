412469fa8fec8db05a7d903eed1d05a7c206c557
Sherry Tseng <iamsherry000@gmail.com>
2025-12-30 16:04:49 +0800

    adjust a little

--- src/ch2_basicBV/BigTwo/V3/BigTwo.java
+++ src/ch2_basicBV/BigTwo/V3/BigTwo.java
@@ -1,16 +1,26 @@
 package ch2_basicBV.BigTwo.V3;
 
+import ch2_basicBV.BigTwo.V3.card.Deck;
+import ch2_basicBV.BigTwo.V3.game.Player;
 import ch2_basicBV.BigTwo.V3.patterns.CardPatternHandler;
 
 public class BigTwo {
     private final CardPatternHandler handlerChain;
-    // private final Deck deck;
+    private Player[] players;
+    private final Deck deck;
 
     public BigTwo(CardPatternHandler handlerChain) {
         this.handlerChain = handlerChain;
+        this.deck = new Deck();
+    }
+
+    public void setPlayers(Player[] players){
+        this.players = players;
     }
 
     public void start() {
         System.out.println("BigTwo started");
+        deck.shuffle();
+        
     }
 }
--- src/ch2_basicBV/BigTwo/V3/Main.java
+++ src/ch2_basicBV/BigTwo/V3/Main.java
@@ -1,21 +1,24 @@
 package ch2_basicBV.BigTwo.V3;
 
-import ch2_basicBV.BigTwo.V3.patterns.SingleHandler;
-import ch2_basicBV.BigTwo.V3.patterns.PairHandler;
-import ch2_basicBV.BigTwo.V3.patterns.StraightHandler;
+import ch2_basicBV.BigTwo.V3.game.Player;
 import ch2_basicBV.BigTwo.V3.patterns.FullHouseHandler;
+import ch2_basicBV.BigTwo.V3.patterns.PairHandler;
+import ch2_basicBV.BigTwo.V3.patterns.SingleHandler;
+import ch2_basicBV.BigTwo.V3.patterns.StraightHandler;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         BigTwo bigTwo = new BigTwo(new SingleHandler(new PairHandler(new StraightHandler(new FullHouseHandler(null)))));
         
         Player[] players = new Player[4];
-        String line = in.nextLine();
+        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < players.length; i++) {
-            String name = in.nextLine();
+            String name = scanner.nextLine();
             players[i] = new Player(name);
         }
         
+        bigTwo.setPlayers(players);
         bigTwo.start();
     }
 }
